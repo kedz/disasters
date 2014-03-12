f01bce52d6be292e5a9f566afd73cfe1084dc2dc
kedz <crk2130@columbia.edu>
2014-03-12 17:29:42 -0400

    Added dependency BeautifulSoup

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 setup(
     name = 'disasters',
     packages = ['disasters'],
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'A python library for my research on disaster news.',
     author='Chris Kedzie',
     author_email='kedzie@cs.columbia.edu',
     url='https://github.com/kedz/disasters',
     install_requires=[
         'corenlp',
+        'BeautifulSoup'
     ]
 
 )
